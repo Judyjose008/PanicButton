7f1cb14de19b86910c947f6709de4cec879bfb3d
judyjose008 <judyjose008@gmail.com>
2019-07-12 11:11:14 +0530

    Changed makerkey

--- IftttLambda.js
+++ IftttLambda.js
@@ -4,14 +4,14 @@
 
 const https = require('https');
 
-//const makerKey = '{{g2neqbQ54kRSiiJ7JTmQeaD7xhgDOQKdiJ2NoRsT033}}'; // change it to your Maker key
+
 
 exports.handler = (event, context, callback) => {
     console.log('Received event:', event);
 
     // make sure you created a receipe for event <serialNumber>-<clickType>
     const makerEvent = `${event.serialNumber}-${event.clickType}`;
-    const url = `https://maker.ifttt.com/trigger/{{makerEvent}}/with/key/{{eventKey}}`;//Replace with your Webhook key from Documentation.
+    const url = `https://maker.ifttt.com/trigger/{{makerEvent}}/with/key/keynumber`;//Replace with your Webhook key from Documentation.
     https.get(url, (res) => {
         let body = '';
         console.log(`STATUS: ${res.statusCode}`);
